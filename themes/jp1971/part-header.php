4d3bb8344bd9905631d2b121bd9117e84ca7e707
Jameson Proctor <jameson@jp1971.com>
2014-01-04 17:21:15 -0500

    Adding header, footer, and analytics parts.

---
+++ themes/jp1971/part-header.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Template for displaying the default site header.
+*/
+?>
+
+<header>
+	<div class="container header">
+		<nav class="navbar" role="navigation">
+			<div class="row">
+				<div class="navbar-brand">
+					<a href="<?php echo home_url(); ?>">
+						<h2>
+							<span>JP1971</span>
+							<br />
+							<small>thinking | making</small>
+						</h2>
+						<span aria-hidden="true" class="jp1971-icomoon-logo"></span>
+					</a>
+				</div>
+			</div>
+		</nav>
+	</div>
+</header>
