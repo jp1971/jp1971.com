fdef48f742cbda056f816f1d5ea6c802fa90e2c1
Jameson Proctor <jameson@jp1971.com>
2014-07-06 15:37:38 -0400

    Stubbing out JS for comments.

--- themes/jp1971/functions/enqueue.php
+++ themes/jp1971/functions/enqueue.php
@@ -7,10 +7,22 @@ add_action( 'wp_enqueue_scripts', 'jp1971_enqueue_styles' );
 function jp1971_enqueue_styles() {
 
    wp_enqueue_style(
-      'reset'//$handle
-      ,get_stylesheet_directory_uri() . "/css/jp1971.css" //$src
-      ,false //$deps (dependencies)
-      ,'1.0' //$ver
-      ,'screen' //$media
+      'jp1971', //$handle
+      get_stylesheet_directory_uri() . "/css/jp1971.css", //$src
+      false, //$deps (dependencies)
+      '1.0', //$ver
+      'screen' //$media
    );
+}
+
+// Print scripts in header and footer
+add_action( 'wp_enqueue_scripts', 'jp1971_enqueue_scripts' );
+function jp1971_enqueue_scripts() {
+	wp_enqueue_script(
+		'jp1971-comments', // $handle
+		get_stylesheet_directory_uri() . "/js/jp1971/comments.js", // $src
+		array( 'jquery' ), // $deps (dependencies)
+		'1.0', // $ver
+		true // $in_footer
+	);
 }
